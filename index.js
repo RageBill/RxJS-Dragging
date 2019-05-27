1b30e7fb944962847eee44ed0b31f1745c650a25
Kelvin Chu <chubillkelvin@gmail.com>
2019-05-27 09:56:07 +0800

    Improve dragging movement.

--- index.js
+++ index.js
@@ -5,17 +5,24 @@ const box = document.querySelector("#draggable");
 
 const mousedown$ = fromEvent(box, "mousedown");
 const mousemove$ = fromEvent(document, "mousemove");
-const mouseup$ = fromEvent(box, "mouseup");
+const mouseup$ = fromEvent(document, "mouseup");
 
-mousedown$.pipe(switchMap(_ => {
+mousedown$.pipe(switchMap(mdEvent => {
+    const startPos = {
+        x: box.getBoundingClientRect().left,
+        y: box.getBoundingClientRect().top,
+    };
     box.innerHTML = "Dragging!";
     return mousemove$.pipe(
-        map(e => ({x: e.x, y: e.y})),
+        map(mmEvent => ({
+            x: mmEvent.x - (mdEvent.x - startPos.x),
+            y: mmEvent.y - (mdEvent.y - startPos.y),
+        })),
         takeUntil(mouseup$)
     )
 })).subscribe(coor => {
-    box.style.left = `${coor.x - 100}px`;
-    box.style.top = `${coor.y - 100}px`;
+    box.style.left = `${coor.x}px`;
+    box.style.top = `${coor.y}px`;
 });
 
 mouseup$.pipe().subscribe(_ => box.innerHTML = "Drag Me!");
